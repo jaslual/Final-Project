0442522e5fb8deb65463105214eceec13509a0ac
niaholmes <niaholme@umich.edu>
2024-11-20 17:47:18 -0500

    Update OpenLibrary.py

--- OpenLibrary.py
+++ OpenLibrary.py
@@ -3,8 +3,9 @@
 # to get information on a book, use this url format: https://openlibrary.org/search.json?q=
 # after the '=' sign, add in the title of the book with the '+' sign in between each word of the book title
 
-# you can search books based on subject (genre) by doing "GET /subjects/love.json", which should return books of the subject 'love',
-# but more specifically, use https://openlibrary.org/subjects/love.json?published_in=2010-2015
+# you can search books based on subject (genre) by doing "GET /subjects/love.json" or "GET /subjects/love.json?details=true", 
+# which should return books of the subject 'love', but more specifically, 
+# use https://openlibrary.org/subjects/love.json?published_in=2010-2015
 
 from olclient import OpenLibrary
 from collections import namedtuple
