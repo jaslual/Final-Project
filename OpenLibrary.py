d8f6acac1349c2cebd0cd8cb2ffc307a1e233a6b
niaholmes <niaholme@umich.edu>
2024-11-20 17:26:05 -0500

    Update OpenLibrary.py

--- OpenLibrary.py
+++ OpenLibrary.py
@@ -1,1 +1,7 @@
 # Open Library GitHub Link: https://github.com/internetarchive/openlibrary-client?tab=readme-ov-file#installation
+
+from olclient import OpenLibrary
+from collections import namedtuple
+Credentials = namedtuple("Credentials", ["username", "password"])
+credentials = Credentials("openlibrary@example.com", "admin123")
+ol = OpenLibrary(base_url="http://localhost:8080", credentials=credentials)
