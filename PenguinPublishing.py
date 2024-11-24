5077c48264556921568d99693424de5d5d5f1505
Jasmine Luallen <jasmine@MacBook-Pro-257.lan>
2024-11-24 17:55:03 -0500

    created fetch_authot_data function

--- PenguinPublishing.py
+++ PenguinPublishing.py
@@ -1,2 +1,17 @@
+import sqlite3
 import requests
+
 API_URL = "https://www.penguinrandomhouse.biz/webservices/rest/resources/authors"
+
+def fetch_author_data(start, max_items=25):
+    params = {
+        'start': start,
+        'max': max_items,
+        'expandLevel': 1,
+    }
+    response = requests.get(API_URL, params=params)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"Error: {response.status_code}, {response.text}")
+        return None
